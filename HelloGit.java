42ef7aac39e7cfe05a6682966fdfdaca5912fd6d
E.v.a.aA.a <eva@eva.pt>
2014-10-09 10:43:11 +0100

    version 1.0

---
+++ HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * Guião 04 - Utilização do Git; ficheiro java para testar a utilização do
+ * controlo de versões usando o git.
+ * 
+ * @author Eva G
+ * @version 1.0 (06/Oct/2014)
+ */
+public class HelloGit
+{
+
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World" );
+		System.out.println( "1st version" );
+	}
+
+}

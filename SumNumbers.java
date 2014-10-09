56c1136f529445af3ebd89640bfb9b2c5971d2bc
E.v.a.aA.a <eva@eva.pt>
2014-10-09 16:07:29 +0100

    more instructional version

--- SumNumbers.java
+++ SumNumbers.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * dois números decimais introduzidos pelo utilizador.
  * 
  * @author Eva G
- * @version 1.0 (09/Oct/2014)
+ * @version 2.0 (09/Oct/2014)
  */
 
 public class SumNumbers
@@ -15,9 +15,12 @@ public class SumNumbers
 	public static void main( String[] args )
 	{
 		Scanner kb = new Scanner( System.in );
-		System.out.print( "Digite dois números: " );
-		double num1 = kb.nextDouble(), num2 = kb.nextDouble();
-		System.out.println( num1 + "+" + num2 + "=" + sum( num1, num2 ) );
+		System.out
+				.print( "Digite dois números (inteiros ou com vírgulas)\nNum1: " );
+		double num1 = kb.nextDouble();
+		System.out.print( "Num2: " );
+		double num2 = kb.nextDouble();
+		System.out.println( num1 + " + " + num2 + " = " + sum( num1, num2 ) );
 	}
 
 	static double sum( double n1, double n2 )
